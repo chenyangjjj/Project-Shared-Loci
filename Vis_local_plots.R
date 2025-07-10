0da803b51feabb4b560df4edcae74ccf184296c9
kookaburra <44012383+chenyangjjj@users.noreply.github.com>
2025-07-10 20:37:28 +0200

    Update Vis_local_plots.R

--- Vis_local_plots.R
+++ Vis_local_plots.R
@@ -5,11 +5,6 @@ library(data.table)
 library(locuszoomr)
 library(EnsDb.Hsapiens.v75)
 library(cowplot)
-library(ggplot2)
-library(data.table)
-library(locuszoomr)
-library(EnsDb.Hsapiens.v75)
-library(cowplot)
 
 main_path = "/home/holstegelab-cjiang/projects/project_AD_shared/"
 # conjFDR results
@@ -25,27 +20,90 @@ data_trait2 <- fread("/home/holstegelab-cjiang/projects/project_AD_shared/data/c
 data_trait3 <- fread("/home/holstegelab-cjiang/projects/project_AD_shared/data/conjFDR_Hipp_rh_biobank_clean.csv")
 
 
-im = 1
+for (im in c(6,2,10)) {
+
 SNP_loci = data_conjFDR$SNP[im]
 CHR_loci = data_conjFDR$chrnum[im]
 BP_floor = data_conjFDR$chrpos[im] - 250000
 BP_ceiling = data_conjFDR$chrpos[im] + 250000
+
+trait1.loc <- locus(data = data_trait1, ens_db = "EnsDb.Hsapiens.v75",
+                    chrom = "CHR", pos = "BP", p = "PVAL",
+                    index_snp = SNP_loci, seqname = CHR_loci,
+                    xrange = c(BP_floor, BP_ceiling))
+trait1.loc <- link_LD(trait1.loc, token = "8e7c11adaa33")
+locus_1 <- gg_scatter(trait1.loc,size = 5,pcutoff = 5e-20) +
+    theme(
+    legend.text = element_text(size = 35),
+    legend.title = element_text(size = 35),
+    axis.title.x = element_blank(),
+    axis.title.y = element_text(size = 35),
+    axis.text.x = element_blank(),
+    axis.text.y = element_text(size = 35),
+    axis.line = element_line(size = 1, colour = "black")
+    )
+track1 = gg_genetracks(trait1.loc,cex.text = 0.7) +
+            theme(
+                legend.text = element_text(size = 35),
+                legend.title = element_text(size = 35),
+                axis.title.x = element_text(size = 35),
+                axis.text.x = element_text(size = 35),
+                axis.line = element_line(size = 1, colour = "black")
+                )
+
+trait2.loc <- locus(data = data_trait2, ens_db = "EnsDb.Hsapiens.v75",
+                    chrom = "CHR", pos = "BP", p = "PVAL",
+                    index_snp = SNP_loci, seqname = CHR_loci,
+                    xrange = c(BP_floor, BP_ceiling))
+trait2.loc <- link_LD(trait2.loc, token = "8e7c11adaa33")
+locus_2 = gg_scatter(trait2.loc,size = 5,pcutoff = 5e-20) +
+    theme(
+    legend.text = element_text(size = 35),
+    legend.title = element_text(size = 35),
+    axis.title.x = element_blank(),
+    axis.title.y = element_text(size = 35),
+    axis.text.x = element_blank(),
+    axis.text.y = element_text(size = 35),
+    axis.line = element_line(size = 1, colour = "black")
+    )
+track2 = gg_genetracks(trait2.loc,cex.text = 0.7) +
+            theme(
+                legend.text = element_text(size = 35),
+                legend.title = element_text(size = 35),
+                axis.title.x = element_text(size = 35),
+                axis.text.x = element_text(size = 35),
+                axis.line = element_line(size = 1, colour = "black")
+                )
+
 trait3.loc <- locus(data = data_trait3, ens_db = "EnsDb.Hsapiens.v75",
                     chrom = "CHR", pos = "BP", p = "PVAL",
                     index_snp = SNP_loci, seqname = CHR_loci,
                     xrange = c(BP_floor, BP_ceiling))
 trait3.loc <- link_LD(trait3.loc, token = "8e7c11adaa33")
-x = gg_scatter(trait3.loc,size = 4) +
+locus_3 = gg_scatter(trait3.loc,size = 5,pcutoff = 5e-20) +
     theme(
-    legend.text = element_text(size = 18),
-    legend.title = element_text(size = 18),
-    axis.title.x = element_text(size = 22),
-    axis.title.y = element_text(size = 22),
-    axis.text.x = element_text(size = 18),
-    axis.text.y = element_text(size = 18),
+    legend.text = element_text(size = 35),
+    legend.title = element_text(size = 35),
+    axis.title.x = element_blank(),
+    axis.title.y = element_text(size = 35),
+    axis.text.x = element_blank(),
+    axis.text.y = element_text(size = 35),
     axis.line = element_line(size = 1, colour = "black")
     )
-gg_addgenes(x, trait3.loc)
-ggsave("/home/holstegelab-cjiang/projects/project_AD_shared/results/plots_rebuttal/gg_scatter.png",x)
+track3 = gg_genetracks(trait3.loc,cex.text = 0.8) +
+            theme(
+                legend.text = element_text(size = 35),
+                legend.title = element_text(size = 35),
+                axis.title.x = element_text(size = 35),
+                axis.text.x = element_text(size = 35),
+                axis.line = element_line(size = 1, colour = "black")
+                )
 
-ggsave("/home/holstegelab-cjiang/projects/project_AD_shared/results/plots_rebuttal/locus_ggplot,png",locus_ggplot(trait3.loc))
+locus_ALL = ggarrange(locus_1,locus_2,locus_3,nrow = 3,ncol = 1)
+ggsave(paste0("/home/holstegelab-cjiang/projects/project_AD_shared/results/plots_rebuttal/gg_scatter_",SNP_loci,".png"),locus_ALL,width = 10,height = 27)
+ggsave(paste0("/home/holstegelab-cjiang/projects/project_AD_shared/results/plots_rebuttal/gg_scatter_",SNP_loci,".pdf"),locus_ALL,width = 10,height = 27,dpi = 350)
+
+ggsave(paste0("/home/holstegelab-cjiang/projects/project_AD_shared/results/plots_rebuttal/gg_track_",SNP_loci,".pdf"),track3,width = 10,height = 3)
+
+
+}
